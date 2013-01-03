241e161cc7e0fa027165639eab0e573c5c3ecdb3
Jordan S Read <jread@usgs.gov>
2013-01-03 15:29:50 -0600

    file zip functional

--- zipLakeFile.m
+++ zipLakeFile.m
@@ -5,22 +5,30 @@ function zipLakeFile(lakeNm)
 
 % - lakeNm must be a string
 
-if ~isstring(lakeNm)
+if ~ischar(lakeNm)
     error('lake name input must be a string')
 end
 
 %% -- variables --
 appendNm = 'WiLMA_';
-rootDir = '/Volumes/projects/WiLMA/';
-zipDir  = '/Volumes/projects/WiLMA/zipp[edLakes/';
-projReWrite = ['GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",'...
+rootDir = '/Volumes/projects/WiLMA/rawLakes/';
+zipDir  = '/Volumes/projects/WiLMA/zippedLakes/';
+prjWrite = ['GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",'...
     'SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM'...
     '["Greenwich",0],UNIT["Degree",0.017453292519943295]]'];
+files = {[lakeNm '.dbf'],[lakeNm '.prj'],...
+    [lakeNm '.shx'],[lakeNm '.shp']};
+replcPrj = true;
 
 %% begin process
-useFiles = dir(fullfile(matlabroot, ...
-               'toolbox/matlab/audiovideo/*.m'))
+if replcPrj
+    fID = fopen([rootDir lakeNm '.prj'],'w');
+    fwrite(fID,prjWrite);
+end
 
+zFile = [zipDir appendNm lakeNm '.zip'];
+
+zip(zFile,files,rootDir)  
 
 
 end
