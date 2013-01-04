76ad7d1dbb440110f5d40a54d2027c15cce5ad4b
Jordan S Read <jread@usgs.gov>
2013-01-04 16:03:03 -0600

    minor fixes to zip

--- zipLakeFile.m
+++ zipLakeFile.m
@@ -1,4 +1,4 @@
-function zipLakeFile(lakeNm,appendNm)
+function zipLakeFile(lakeNm,appendNm,rootDir,zipDir)
 % aggregates names, appends a name to the begining of the zip file,
 % re-writes the .prj file for each lake (to conform to prj2epsg web
 % services)
@@ -7,6 +7,8 @@ function zipLakeFile(lakeNm,appendNm)
 
 if eq(nargin,1)
     appendNm = 'WiLMA_lake_';
+    rootDir = '/Volumes/projects/WiLMA/rawLakes/';
+    zipDir  = '/Volumes/projects/WiLMA/zippedLakes/';
 end
 
 if ~ischar(lakeNm)
@@ -15,8 +17,7 @@ end
 
 %% -- variables --
 
-rootDir = '/Volumes/projects/WiLMA/rawLakes/';
-zipDir  = '/Volumes/projects/WiLMA/zippedLakes/';
+
 prjWrite = ['GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",'...
     'SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM'...
     '["Greenwich",0],UNIT["Degree",0.017453292519943295]]'];
