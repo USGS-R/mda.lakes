12647bd85090804ae63d7fb5a6748398fe589b25
Jordan S Read <jread@usgs.gov>
2013-07-23 14:14:45 -0500

    added getClarity, tested vs matlab

--- R/GLM.functions.R
+++ R/GLM.functions.R
@@ -8,10 +8,27 @@ getArea	<-	function(WBIC){
 }
 
 getCanopy	<-	function(WBIC){
+	data	<-	read.table('../supporting files/VegetationHeight_WiLMA.tsv',header=TRUE,sep='\t')
+	indx	<-	paste(c('X',WBIC),collapse='')
+	if (indx %in% names(data)){
+		canopy.height	<-	as.numeric(levels(data[1,indx])[1])
+	} else {
+		canopy.height	<-	NULL
+	}
 	return(canopy.height)
 }
 
 getClarity	<-	function(WBIC){
+	secchiConv	<-	1.7
+	data	<-	read.table('../supporting files/annual_mean_secchi.txt',header=TRUE,sep='\t')
+	useI	<- data['WBIC']==WBIC
+	if (any(useI)){
+		secchi 	<-	data$secchi.m.mean[useI]
+		attenuation.coefficient	<-	secchiConv/mean(secchi,na.rm=TRUE)
+	} else {
+		attenuation.coefficient	<-	NULL
+	}
+	
 	return(attenuation.coefficient)
 }
 
