a87dea3083c739d773b9fbf8e8247c37d3c76120
Jordan S Read <jread@usgs.gov>
2013-07-23 13:46:05 -0500

    initial commit

---
+++ R/GLM.functions.R
@@ -0,0 +1,38 @@
+getBathy	<-	function(WBIC){
+	
+	return(bathymetry)
+}
+
+getArea	<-	function(WBIC){
+	return(surface.area)
+}
+
+getCanopy	<-	function(WBIC){
+	return(canopy.height)
+}
+
+getClarity	<-	function(WBIC){
+	return(attenuation.coefficient)
+}
+
+getElevation	<-	function(WBIC){
+	return(elevation)
+}
+
+getLatitude	<-	function(WBIC){
+	return(latitude)
+}
+
+getLongitude<-	function(WBIC){
+	return(longitude)
+}
+
+getWstr	<-	function(WBIC){
+	return(wind.shelter)
+}
+
+getZmax	<-	function(WBIC){
+	return(max.depth)
+}
+
+
