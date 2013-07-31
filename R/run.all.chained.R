93cd8bacb1acb55835d3dd813a46932234eb2951
Jordan S Read <jread@usgs.gov>
2013-07-31 08:56:17 -0500

    reverting params

--- R/run.all.chained.R
+++ R/run.all.chained.R
@@ -18,6 +18,7 @@ glm.path = "C:/Users/jread/Desktop/GLM_v1.2.0/bin/glm.exe"
 model.ids = basename(model.dirs)
 WBICs = str_extract(model.ids,'\\d+')  # WBICS as strings
 
+wndRef = 0.0015
 
 
 empir.ice = read.table('../supporting files/empirical.ice.tsv', sep='\t', header=TRUE, as.is=TRUE)
@@ -49,8 +50,8 @@ for(i in 1:length(model.ids)){
   #glm.path must be absolute path, not relative
   
   Wstr = getWstr(WBICs[i],method='JEKL')
-  nml.args = list('coef_wind_drag'=0.0016*Wstr^0.33)
-  print(nml.args)
+  nml.args = list('coef_wind_drag'=wndRef*Wstr^0.33,'coef_mix_conv'=0.125,'ce'=0.00142)# reverting...
+  
   run.chained.GLM(model.dirs[i], glm.path = glm.path,nml.args, verbose=FALSE)
   
   ## Now use calibration data to output matched modeled data for validation
@@ -72,6 +73,7 @@ for(i in 1:length(model.ids)){
     print(paste(c('linear model standard error:',summary(lm)$sigma),collapse=' '))
     plot(dat$WTEMP,dat$WTEMP_MOD)
     lines(c(0,30),c(0,30))
+    print(paste(nml.args$coef_wind_drag,'vs Markfort',getWstr(WBICs[i])^0.33*wndRef,sep=' '))
   } else {stdErr = NA}
  
   print(paste(c('obs vs model standard error:',stdErr,', from',length(resids),'points'),collapse=' '))
