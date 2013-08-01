01f4cbabe306528eca39ed2fc5fa485f9a21b772
Jordan S Read <jread@usgs.gov>
2013-08-01 12:48:36 -0500

    function for estimating SE

--- R/est.error.R
+++ R/est.error.R
@@ -1,1 +1,29 @@
-est.error
+# load lake summary document, which holds WBIC and SE
+
+source('GLM.functions.R')
+
+dat <-	read.table(file=fileName,header=TRUE,sep='\t')
+n.cut 	<- 20	# data points required for analysis
+# remove NAs
+WBIC	<-	dat$WBIC
+SE	<-	dat$StndErr
+n	<-	dat$numPoints
+
+rmvI	<-	(is.na(SE) | n < n.cut)
+WBIC	<-	WBIC[!rmvI]
+n	<-	n[!rmvI]
+SE	<-	SE[!rmvI]
+n.lakes	<-	length(SE)
+
+area	<-	vector(length=n.lakes)
+SDF	<-	vector(length=n.lakes)
+depth	<-	vector(length=n.lakes)
+res.time	<-	vector(length=n.lakes)
+canopy	<-	vector(length=n.lakes)
+clarity	<-	vector(length=n.lakes)
+
+for (i in 1:n.lakes){
+	area[i]	<-	getArea(WBIC[i])
+	SDF[i]	<-	getSDF(WBIC[i])
+}
+
