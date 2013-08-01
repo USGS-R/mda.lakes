c3dfdf02c6257f49ca80d7975d40fb460984fb2d
Jordan S Read <jread@usgs.gov>
2013-08-01 16:01:33 -0500

    trying other estimates of SE

--- R/est.error.R
+++ R/est.error.R
@@ -1,16 +1,17 @@
 # load lake summary document, which holds WBIC and SE
 
 source('GLM.functions.R')
-
+fileName	<-	'../supporting files/summary.txt'
 dat <-	read.table(file=fileName,header=TRUE,sep='\t')
 n.cut 	<- 20	# data points required for analysis
 # remove NAs
-WBIC	<-	dat$WBIC
+WBICs	<-	as.character(dat$WBIC)
 SE	<-	dat$StndErr
 n	<-	dat$numPoints
 
+
 rmvI	<-	(is.na(SE) | n < n.cut)
-WBIC	<-	WBIC[!rmvI]
+WBICs	<-	WBICs[!rmvI]
 n	<-	n[!rmvI]
 SE	<-	SE[!rmvI]
 n.lakes	<-	length(SE)
@@ -21,9 +22,24 @@ depth	<-	vector(length=n.lakes)
 res.time	<-	vector(length=n.lakes)
 canopy	<-	vector(length=n.lakes)
 clarity	<-	vector(length=n.lakes)
+perim	<-	vector(length=n.lakes)
+Wstr	<-	vector(length=n.lakes)
 
 for (i in 1:n.lakes){
-	area[i]	<-	getArea(WBIC[i])
-	SDF[i]	<-	getSDF(WBIC[i])
+	area[i]	<-	getArea(WBICs[i])
+	perim[i]	<-	getPerim(WBICs[i])
+	SDF[i]	<-	getSDF(WBICs[i])
+	depth[i]	<-	getZmax(WBICs[i])
+	#res.time[i]
+	canopy[i]	<- getCanopy(WBICs[i])
+	Wstr[i]	<-	getWstr(WBICs[i])
+	clarity[i]	<-	getClarity(WBICs[i])
 }
 
+perSq.area	<-	sqrt(perim)/area
+lm	<-	lm(SE~perSq.area-1)
+
+plot(SE,perSq.area*17541.3)
+
+summary(lm)
+#plot(lm)
