fc610ca199ff2e264e04a6f9fbbe215b0507ad3d
Jordan S Read <jread@usgs.gov>
2014-01-07 12:31:31 -0600

    mods and cleanup to functions

--- R/calc.strat.onset.R
+++ R/calc.strat.onset.R
@@ -1,9 +1,8 @@
-source("random.error.temp.R")
 source("GLM.functions.R")
 require(rGLM)
 lyrDz	<-	0.25
-year	<-	'2009'
-file.out <- paste("../supporting files/strat.onset",year,'.tsv',sep='')
+#year	<-	'2009'
+
 
 calc.strat.onset	<-	function(WBIC,year,min.duration=30,mix.diff=1.0){
 	# min.duration is in TIME UNITS
@@ -20,14 +19,12 @@ calc.strat.onset	<-	function(WBIC,year,min.duration=30,mix.diff=1.0){
 	  	temp.surf	<-	getTempGLMnc(GLMnc,lyrDz,ref='surface',z.out=0)
 		temp.bot	<-	getTempGLMnc(GLMnc,lyrDz,ref='bottom',z.out=0)
 	  	nc_close(GLMnc)
-		plot(temp.surf[,2])
-		lines(temp.bot[,2])
 		strat.count	<-	0
 		for (j in 1:length(temp.surf$DateTime)){
 			if (temp.surf[j,2]-temp.bot[j,2]>mix.diff){
 				strat.count = strat.count+1
 				if (strat.count>=min.duration){
-					strat.onset	<-	as.numeric(strftime(temp.surf[j,1], format = "%j"))
+					strat.onset	<-	as.numeric(strftime(temp.surf[j,1], format = "%j"))-min.duration
 					break
 					}
 			} else {
@@ -38,21 +35,35 @@ calc.strat.onset	<-	function(WBIC,year,min.duration=30,mix.diff=1.0){
 	return(strat.onset)
 }
 
-lake.ids <- get.lakes(year=year)
-num.lakes <- length(lake.ids)
-lake.ids <- lake.ids
-strat.matrix <- array(dim=c(num.lakes,1) #
-
-for (j in 1:num.lakes){
-	cat('WBIC ');cat(WBIC); cat(' processing.')
-	strat.matrix[j]	<-	calc.strat.onset(lake.ids[j],year)
-	cat('.')
+get.lakes <- function(year){
+  # if doing more than a few years, this should be stripped for just one column and kept in memory (future)
+  dat<-read.delim('../supporting files/omg.huge.output.tsv',header = TRUE, sep = "\t")
+  # get rid of all rows that don't match the year
+  use.idx <- which(dat$year==year & !is.na(dat$dateOver8.9))
+  dat <- dat[use.idx,]
+  lake.ids <- as.character(dat$lakeid)
+  return(lake.ids)
 }
 
-write.out <- data.frame(lake.ids=lake.ids,strat.matrix)
-# need to name the columns
-col.names	<-	names(write.out)
-col.names	<-	c("WBIC","strat.onset.DoY")
+call.lakes	<-	function(year){
+	file.out <- paste("../supporting files/strat.onset",year,'.tsv',sep='')
+	lake.ids <- get.lakes(year=year)
+	num.lakes <- length(lake.ids)
 
-names(write.out) <- col.names
-write.table(x=write.out,file=file.out,sep='\t',col.names=TRUE,quote=FALSE,row.names = FALSE)
+	strat.matrix <- array(dim=c(num.lakes,1)) #
+
+	for (j in 1:num.lakes){
+		WBIC = lake.ids[j]
+		strat.matrix[j]	<-	calc.strat.onset(WBIC,year)
+		cat('.\n')
+	}
+
+	write.out <- data.frame(lake.ids=lake.ids,strat.matrix)
+	# need to name the columns
+	col.names	<-	names(write.out)
+	col.names	<-	c("WBIC","strat.onset.DoY")
+
+	names(write.out) <- col.names
+	write.table(x=write.out,file=file.out,sep='\t',col.names=TRUE,quote=FALSE,row.names = FALSE)
+}
+call.lakes(1998)
