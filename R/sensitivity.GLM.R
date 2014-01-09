edf3214ac90378023c5b7ceb65cc2924e5036045
Jordan S Read <jread@usgs.gov>
2014-01-09 07:27:06 -0600

    full run all lakes 'Kw'

--- R/sensitivity.GLM.R
+++ R/sensitivity.GLM.R
@@ -22,6 +22,7 @@ sensitivity.GLM	<-	function(model.dirs,param,range,year,n=10,driver.dir='D:/WiLM
 	origin	<-	getwd()
 	
 	for (j in 1:num.lakes){
+    cat(model.ids[j])
 		driver.file	<-	paste(driver.dir, '/', model.ids[j], '.csv', sep='')
 		file.copy(driver.file, model.dirs[j])
 		setwd(model.dirs[j])	# set to this lake's run directory
@@ -38,12 +39,15 @@ sensitivity.GLM	<-	function(model.dirs,param,range,year,n=10,driver.dir='D:/WiLM
 			out = system2(glm.path, wait=TRUE, stdout=TRUE,stderr=TRUE)	# runs and writes .nc to directory
       hypo.temps<- get.sim.temps(run.dir[j])
 			response.matrix[j,i]	<-	 mean(hypo.temps[,2],na.rm=TRUE)
-      cat('done with j=');cat(j);cat(' and i=');cat(i);cat('\n')
+      cat('done with j=');cat(j);cat('of ');cat(num.lakes);cat(' and i=');cat(i);cat('\n')
 		}
 		file.rename('glm.nml.orig', 'glm.nml')
     setwd(origin)
 		
 	}	
+	response.matrix = data.frame(response.matrix)
+  names(response.matrix) <- paste(param,'_',new.params,sep='')
+  response.matrix <- cbind("WBICs"=WBICs,response.matrix)
   return(response.matrix)
 }
 
@@ -53,14 +57,13 @@ get.sim.temps	<-	function(run.dir,remove=FALSE){
   lyrDz <- 0.25
   GLMnc  <-  getGLMnc(file='output.nc',folder='./')
   temps	<-	getTempGLMnc(GLMnc,lyrDz,ref='bottom',z.out=0) # DO JAS MEAN?
+  nc_close(GLMnc)
 	if (remove){
 		# delete nc file ...
 	}
 	return(temps)
 }
 
-param = 'Kw'
-response.matrix <- sensitivity.GLM(model.dirs,param='Kw',range=c(0.2,5),year=1996,n=3)
-WBICs  <-	str_extract(model.ids,'\\d+')  # WBICS as strings
-write.out <- cbind(WBICs,response.matrix)
-write.table(write.out,file=paste('sensitivity_',param,'.tsv',sep=''),quote=FALSE,sep='\t',row.names=FALSE)
+response.matrix <- sensitivity.GLM(model.dirs,param='Kw',range=c(0.2,5),year=1996,n=10)
+
+write.table(response.matrix,file=paste('sensitivity_',param,'.tsv',sep=''),quote=FALSE,sep='\t',row.names=FALSE)
