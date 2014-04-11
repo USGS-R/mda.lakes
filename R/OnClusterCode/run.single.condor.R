c8b40fc2718a709c0704d7f00889b4da6f8c0fe8
Luke Winslow <lawinslow@gmail.com>
2014-04-11 15:17:44 -0500

    Added a bunch of new on cluster code (and new rGLM version)

--- R/OnClusterCode/run.single.condor.R
+++ R/OnClusterCode/run.single.condor.R
@@ -3,7 +3,7 @@
 
 
 install.packages("ncdf4_1.4.zip", lib='./rLibs', repos=NULL)
-install.packages("rGLM_0.1.2.tar.gz", lib='./rLibs', repos=NULL, type='source')
+install.packages("rGLM_0.1.5.tar.gz", lib='./rLibs', repos=NULL, type='source')
 source('chained.GLM.lib.R')
 library(rGLM)
 
