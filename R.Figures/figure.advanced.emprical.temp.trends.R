c7457cf55b82e04b734e3fda4c0042a5a37c98a4
Luke Winslow <lawinslow@gmail.com>
2014-06-15 16:04:16 -0500

    Fixed error with calling unique on data.table (was using only WBIC to determine unique rows).

--- R.Figures/figure.advanced.emprical.temp.trends.R
+++ R.Figures/figure.advanced.emprical.temp.trends.R
@@ -2,6 +2,7 @@
 library(plyr)
 library(data.table)
 
+
 all.slopes.perms = function(times, data){
 	
 	if(length(data)==2){
@@ -11,6 +12,7 @@ all.slopes.perms = function(times, data){
 	perm.i = combn(1:length(data),2)
 	#perm1 = data[perm.i[1,],]
 	#perm2 = data[perm.i[2,],]
+	#browser()
 	
 	return((data[perm.i[1,]]-data[perm.i[2,]])/(times[perm.i[1,]] - times[perm.i[2,]]))
 }
@@ -26,20 +28,26 @@ wtr[, week:= floor((as.POSIXlt(datetime)$yday+1)/7)]
 wtr[, year:=as.POSIXlt(datetime)$year+1900]
 
 #wtr = ddply(wtr, c('wbic', 'datetime'), function(df) df[which.min(df$depth),])
-wtr = data.table(wtr)
+#wtr = data.table(wtr)
 wtr[,depth:=ceiling(depth)] #set all depths to the deeper whole meter
 
+setkey(wtr, wbic, week, depth)
 u.events = unique(wtr[,list(wbic, week, depth)])
 
-setkey(wtr, wbic, datetime, depth)
+#setkey(wtr, wbic, datetime, depth)
+
 
 all.slopes = data.table()
-
+n = 0
 for(i in 1:nrow(u.events)){
+	
+	if(i%%100 == 0){
+		cat(100*i/nrow(u.events), 'percent done\n')
+	}
 	
 	u.event = u.events[i,]
 	
-	moment = wtr[wbic==u.events[i,]$wbic & week==u.events[i,]$week & depth==u.events[i,]$depth, ]
+	moment = wtr[wbic==u.event$wbic & week==u.event$week & depth==u.event$depth, ]
 	
 	if(nrow(moment) < 2){
 		next
@@ -49,9 +57,7 @@ for(i in 1:nrow(u.events)){
 	
 	all.slopes = rbind(all.slopes, 
 										 merge(u.event,data.table(slopes=slopes, wbic=u.event$wbic), by='wbic'))
-	if(i%%100 == 0){
-		cat(100*i/nrow(u.events), 'percent done\n')
-	}
+	n = n + nrow(moment)
 }
 
 all.slopes = all.slopes[!is.infinite(slopes) & !is.na(slopes),]
