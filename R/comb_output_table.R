de9cc9ecc91e99d4724431e28530f1c0b3193342
Luke Winslow <lwinslow@usgs.gov>
2014-11-14 14:19:26 -0600

    Added useful function for combining output data.

---
+++ R/comb_output_table.R
@@ -0,0 +1,28 @@
+#'
+#'@title Combine file-based table output
+#'
+#'@param pattern Pattern to be passed to \code{\link{Sys.glob}}
+#'@param ... Additional parameters passed to \code{\link{read.table}}
+#'
+#'@description
+#'Attempts to load all files matching a certain pattern and combine
+#'them vertically (rbind). Returns the result. If no files match the 
+#'pattern, then an empty data.frame is returned.
+#'
+#'@author Luke Winslow
+#'
+#'
+#'@export
+comb_output_table = function(pattern, ...){
+	
+	files = Sys.glob(pattern)
+	
+	out = data.frame()
+	
+	for(i in 1:length(files)){
+		tmp = read.table(files[i], ...)
+		out = rbind(out, tmp)
+	}
+	
+	return(out)
+}
