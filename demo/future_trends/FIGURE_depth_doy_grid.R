fe5f461058e56a27e5bc5b8af52027ef0dbbb1cb
Luke Winslow <lwinslow@usgs.gov>
2015-09-01 16:39:23 -0500

    local changes to merge

--- demo/future_trends/FIGURE_depth_doy_grid.R
+++ demo/future_trends/FIGURE_depth_doy_grid.R
@@ -5,15 +5,16 @@ library(dplyr)
 library(rLakeAnalyzer)
 library(reshape2)
 library(lubridate)
+library(mda.lakes)
 #Analyze!
 
-load('~/FUTURE_GENMOM.Rdata')
+#load('~/FUTURE_GENMOM.Rdata')
 site_ids = unlist(lapply(dframes, function(l){l$site_id[1]}))
 
-bathy = getBathy('805400')
-names(bathy) = c('depths', 'areas')
+#bathy = getBathy('805400')
+#names(bathy) = c('depths', 'areas')
 
-tmp = dframes[[which(site_ids=='WBIC_805400')]]
+tmp = dframes[[which(site_ids=='WBIC_1881900')]]
 tmp$site_id = NULL
 tmp$DateTime = as.POSIXct(tmp$DateTime)
 names(tmp) = tolower(names(tmp))
@@ -25,24 +26,29 @@ wtr_sens = sens_seasonal_site(year(long$datetime), long$temp, yday(long$datetime
 
 grid_df = ddply(wtr_sens, c('sites_i', 'season_i'), function(df){data.frame(slope=median(df$slopes, na.rm=TRUE), num=nrow(df))})
 
-grid_df = grid_df[grid_df$num > 1000,]
+grid_df = grid_df[grid_df$num > 100,]
 
 grid_mat = dcast(grid_df, sites_i~season_i, median, value.var='slope')
+grid_mat = grid_mat[complete.cases(grid_mat), ]
 
 y = grid_mat[,1]
 x = as.numeric(names(grid_mat)[-1])
 
-png('~/grid.sens.me.png', res=300, width=2000, height=1500)
-image(x, y, t(as.matrix(grid_mat[,-1])), ylim=rev(range(y)), col=rev(heat.colors(12)))
+grid_mat = grid_mat[,-1]
+cols = colorRampPalette(colors=c('blue','white', 'red'))(12)
+
+png('~/nldas.grid.sens.sp.png', res=300, width=2000, height=1500)
+image(x, y, t(as.matrix(grid_mat)), ylim=rev(range(y)), col=cols, zlim=c(-0.1,0.1), 
+      xlab='Day of Year', ylab='Depth')
 dev.off()
 
-png('~/season.sens.me.png', res=300, width=2000, height=1500)
-boxplot(slopes~season_i, wtr_sens, ylim=c(-0.1, 0.1))
+png('~/nldas.season.sens.me.png', res=300, width=2000, height=1500)
+boxplot(slopes~season_i, wtr_sens, ylim=c(-0.1, 0.1), xlab="Day of Year", ylab='Trend')
 abline(0,0)
 dev.off()
 
-png('~/depths.sens.me.png', res=300, width=2000, height=1500)
-boxplot(slopes~sites_i, wtr_sens, ylim=c(-0.1, 0.1))
+png('~/nldas.depths.sens.me.png', res=300, width=2000, height=1500)
+boxplot(slopes~sites_i, wtr_sens, ylim=c(-0.1, 0.1), ylab='Trend', xlab="Depth")
 abline(0,0)
 dev.off()
 
--- demo/future_trends/FIGURE_met_sens_trend.R
+++ demo/future_trends/FIGURE_met_sens_trend.R
@@ -12,11 +12,11 @@ vars = names(driver)[-1]
 
 for(i in 1:length(vars)){
 
-  driver_slopes = sens_seasonal_site(year(driver$time), data = driver[,vars[i]], season_i = yday(driver$time))
+  driver_slopes = sens_seasonal_site(year(driver$time), data = driver[,vars[i]], season_i = week(driver$time))
   
   season_slopes = ddply(driver_slopes, 'season_i', function(df){data.frame(med=median(df$slopes))})
   
-  png(paste0('~/season.sens.', vars[i],'.png'), res=300, width=2000, height=1500)
+  png(paste0('~/season.week.sens.', vars[i],'.png'), res=300, width=2000, height=1500)
   plot(season_slopes, type='l')
   abline(0,0)
   #boxplot(slopes~season_i, driver_slopes, main=vars[i])
